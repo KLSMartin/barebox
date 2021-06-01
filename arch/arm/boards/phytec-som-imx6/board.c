3e314e88de1698e166ee5ef5e272b1dd9519f205
Leif Middelschulte <Leif.Middelschulte@klsmartin.com>
2021-06-01 15:03:27 +0200

    phytec-som-imx6:board.c: fix string duplication
    There is no need to use string format interpolation.

--- arch/arm/boards/phytec-som-imx6/board.c
+++ arch/arm/boards/phytec-som-imx6/board.c
@@ -232,7 +232,7 @@ static int physom_imx6_probe(struct device *dev)
 		envdev = "SPI NOR flash";
 		break;
 	default:
-		environment_path = strdup(default_environment_path);
+		environment_path = xstrdup(default_environment_path);
 		envdev = default_envdev;
 		break;
 	}
